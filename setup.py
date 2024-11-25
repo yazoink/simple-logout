e8122bb4e52d2e9b30122ea30619d3ceed616259
yazoink <yazoink@firemail.cc>
2024-11-26 01:03:44 +1100

    rename file

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='simple-logout',
     version='1.0',
-    scripts=['simple-logout.py'],
+    scripts=['simple-logout'],
     install_requires=['PyGObject3', 'sh']
 )
